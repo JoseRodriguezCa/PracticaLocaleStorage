435e7490107e0c237aa67017595ffc80ce62d0cb
Jose Rodriguez <128047100+JoseRodriguezCa@users.noreply.github.com>
2024-05-13 12:51:51 +0200

    a
    a

--- index.js
+++ index.js
@@ -84,137 +84,137 @@ const PRODUCTS = [
 const carritoLleno = JSON.parse(localStorage.getItem("productsCart")) || [];
 
 const printProductsContent = (products) => {
-  const divContent = document.querySelector(".content");
-  divContent.innerHTML = "";
+const divContent = document.querySelector(".content");
+divContent.innerHTML = "";
 
-  for (const product of products) {
-    const div = document.createElement("div");
-    const name = document.createElement("h3");
-    const price = document.createElement("p");
-    const divImg = document.createElement("div");
-    const img = document.createElement("img");
-    const cart = document.createElement("img");
-    const p = document.createElement('span')
-    name.textContent = product.nombre;
-    price.textContent = product.precio;
-    img.src = product.img;
-    divImg.classList.add("div-img");
-    div.classList.add("product");
-    cart.classList.add("cart-img");
-    cart.src = "https://cdn-icons-png.flaticon.com/512/5465/5465858.png";
-    div.append(cart);
-    div.append(name);
-    div.append(divImg);
-    div.append(price);
-    divImg.append(img);
-    divContent.append(div);
-    if(carritoLleno){
-      const totalSumElement = document.querySelector("#totalSum");
-      totalSumElement.style.display = "none";
-    }
-    cart.addEventListener("click", () => {
-      const existingProduct = carritoLleno.find(item => item.id === product.id);
-      if (existingProduct) {
-        existingProduct.cantidad += 1;
-      } else {
-        carritoLleno.push(product);
-      }
-      localStorage.setItem("productsCart", JSON.stringify(carritoLleno));
-      actualizarCarrito(carritoLleno);
-    });
+for (const product of products) {
+  const div = document.createElement("div");
+  const name = document.createElement("h3");
+  const price = document.createElement("p");
+  const divImg = document.createElement("div");
+  const img = document.createElement("img");
+  const cart = document.createElement("img");
+  const p = document.createElement('span')
+  name.textContent = product.nombre;
+  price.textContent = product.precio;
+  img.src = product.img;
+  divImg.classList.add("div-img");
+  div.classList.add("product");
+  cart.classList.add("cart-img");
+  cart.src = "https://cdn-icons-png.flaticon.com/512/5465/5465858.png";
+  div.append(cart);
+  div.append(name);
+  div.append(divImg);
+  div.append(price);
+  divImg.append(img);
+  divContent.append(div);
+  if(carritoLleno){
+    const totalSumElement = document.querySelector("#totalSum");
+    totalSumElement.style.display = "none";
   }
+  cart.addEventListener("click", () => {
+    const existingProduct = carritoLleno.find(item => item.id === product.id);
+    if (existingProduct) {
+      existingProduct.cantidad += 1;
+    } else {
+      carritoLleno.push(product);
+    }
+    localStorage.setItem("productsCart", JSON.stringify(carritoLleno));
+    actualizarCarrito(carritoLleno);
+  });
+}
 };
 
 const carrito = document.querySelector(".carrito");
 
 carrito.addEventListener("click", () => {
-  const cartDiv = document.querySelector(".cart");
-  const body$$ = document.body
-  cartDiv.classList.toggle("openned");
-  body$$.classList.toggle("openned");})
+const cartDiv = document.querySelector(".cart");
+const body$$ = document.body
+cartDiv.classList.toggle("openned");
+body$$.classList.toggle("openned");})
 
 
 const cart = document.querySelector(".cart");
 cart.innerHTML = "";
 
 const actualizarCarrito = (products) => {
-  cart.innerHTML = "";
-  let totalSumValue = 0;
-  
-  products.forEach(product => {
-      const divProduct = document.createElement("div");
-      const imgProduct = document.createElement("img");
-      const divSpan = document.createElement('div')
-      const numberSpan = document.createElement('span')
-      const lessSpan = document.createElement('span')
-      const sumSpan = document.createElement('span')
-      const cartTotal = document.createElement('span')
-      imgProduct.src = product.img;
-      divProduct.className = 'cart-div'
-      divSpan.className = "div-span"
-      numberSpan.textContent = product.cantidad;
-      const totalPrice = product.precio * product.cantidad;
-      cartTotal.textContent = totalPrice + "€";
-      totalSumValue += totalPrice;
-      lessSpan.classList = "fa-solid fa-minus";
-      sumSpan.classList = "fa-solid fa-plus";
-      divSpan.append(lessSpan, numberSpan, sumSpan);
-      divProduct.append(imgProduct, divSpan, cartTotal);
-      cart.append(divProduct);
-      lessSpan.addEventListener("click", () => restarProducto(product, divProduct, numberSpan, cartTotal, products));
-      sumSpan.addEventListener("click", () => sumarProducto(product, numberSpan, cartTotal, products));
-  });
-      const totalSum = document.createElement('span');
-      totalSum.id = 'totalSum'
-      totalSum.textContent = "Total: " + totalSumValue.toFixed(2) + "€"; 
-      cart.append(totalSum); 
-      actualizarPrecioTotal(products);
+cart.innerHTML = "";
+let totalSumValue = 0;
+
+products.forEach(product => {
+    const divProduct = document.createElement("div");
+    const imgProduct = document.createElement("img");
+    const divSpan = document.createElement('div')
+    const numberSpan = document.createElement('span')
+    const lessSpan = document.createElement('span')
+    const sumSpan = document.createElement('span')
+    const cartTotal = document.createElement('span')
+    imgProduct.src = product.img;
+    divProduct.className = 'cart-div'
+    divSpan.className = "div-span"
+    numberSpan.textContent = product.cantidad;
+    const totalPrice = product.precio * product.cantidad;
+    cartTotal.textContent = totalPrice + "€";
+    totalSumValue += totalPrice;
+    lessSpan.classList = "fa-solid fa-minus";
+    sumSpan.classList = "fa-solid fa-plus";
+    divSpan.append(lessSpan, numberSpan, sumSpan);
+    divProduct.append(imgProduct, divSpan, cartTotal);
+    cart.append(divProduct);
+    lessSpan.addEventListener("click", () => restarProducto(product, divProduct, numberSpan, cartTotal, products));
+    sumSpan.addEventListener("click", () => sumarProducto(product, numberSpan, cartTotal, products));
+});
+    const totalSum = document.createElement('span');
+    totalSum.id = 'totalSum'
+    totalSum.textContent = "Total: " + totalSumValue.toFixed(2) + "€"; 
+    cart.append(totalSum); 
+    actualizarPrecioTotal(products);
 }
 
 const restarProducto = (producto, divProducto, spanCantidad, cartTotal, products) => {
-    if (producto.cantidad > 1) {
-        producto.cantidad--;
-        spanCantidad.textContent = producto.cantidad;
-        cartTotal.textContent = (producto.precio * producto.cantidad).toFixed(2) + "€"
-        
-    } else {
-        const index = carritoLleno.findIndex(item => item.id === producto.id);
-        if (index !== -1) {
-            carritoLleno.splice(index, 1);
-        }
-        divProducto.remove();
-        if(carritoLleno.length === 0){
-        const totalSumElement = document.querySelector("#totalSum");
-        totalSumElement.remove()
-        }
-    }
-    guardarCarrito();
-    actualizarPrecioTotal(products)
+  if (producto.cantidad > 1) {
+      producto.cantidad--;
+      spanCantidad.textContent = producto.cantidad;
+      cartTotal.textContent = (producto.precio * producto.cantidad).toFixed(2) + "€"
+      
+  } else {
+      const index = carritoLleno.findIndex(item => item.id === producto.id);
+      if (index !== -1) {
+          carritoLleno.splice(index, 1);
+      }
+      divProducto.remove();
+      if(carritoLleno.length === 0){
+      const totalSumElement = document.querySelector("#totalSum");
+      totalSumElement.remove()
+      }
+  }
+  guardarCarrito();
+  actualizarPrecioTotal(products)
 }
 
 const sumarProducto = (producto, spanCantidad, totalSpan, products) => {
-    producto.cantidad++;
-    spanCantidad.textContent = producto.cantidad;
-    totalSpan.textContent = (producto.precio * producto.cantidad).toFixed(2) + "€"
-    guardarCarrito();
-    actualizarPrecioTotal(products)
+  producto.cantidad++;
+  spanCantidad.textContent = producto.cantidad;
+  totalSpan.textContent = (producto.precio * producto.cantidad).toFixed(2) + "€"
+  guardarCarrito();
+  actualizarPrecioTotal(products)
 }
 
 const actualizarPrecioTotal = (products) => {
-  let totalSumValue = 0; 
-  
-  
-  for (const product of products) {
-      totalSumValue += product.precio * product.cantidad;
-  }
-  
-  
-  const totalSumElement = document.querySelector("#totalSum");
-  totalSumElement.textContent = "Total: " + totalSumValue.toFixed(2) + "€";
+let totalSumValue = 0; 
+
+
+for (const product of products) {
+    totalSumValue += product.precio * product.cantidad;
+}
+
+
+const totalSumElement = document.querySelector("#totalSum");
+totalSumElement.textContent = "Total: " + totalSumValue.toFixed(2) + "€";
 }
 
 const guardarCarrito = () => {
-    localStorage.setItem("productsCart", JSON.stringify(carritoLleno));
+  localStorage.setItem("productsCart", JSON.stringify(carritoLleno));
 }
 
 actualizarCarrito(carritoLleno);
